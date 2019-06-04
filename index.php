7f003eb41522fe9ea2c654a7b70b65d35438d26d
motomotoyoshi <motomoto.git@gmail.com>
2019-06-04 15:14:20 +0000

    built in function

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php 
 
-function sayHi($name = "hoge"){
-  //echo "hi! " . $name;
-  return "hi! " . $name;
-}
+$x = 5.6;
+//echo ceil($x);
+//echo floor($x);
+//echo round($x);
+//echo rand(1, 10);
 
-//sayHi();
-//sayHi("Tom");
-//sayHi("Bob");
-//sayHi();
+$s1 = "hell";
+$s2 = "くま";
+//;echo strlen($s1);
+//;echo mb_strlen($s2);
+//;printf("%s - %s - %.3f", $s1, $s2, $x);
 
-$s = sayHi();
-var_dump($s);
+$colors = ["red", "pink", "orange"];
+echo count($colors);
+echo implode(",", $colors);
