a17c728aa72704617a9dae64084ca57df6e6d75f
motomotoyoshi <motomoto.git@gmail.com>
2019-06-05 09:29:34 +0000

    class

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php 
 
-$x = 5.6;
-//echo ceil($x);
-//echo floor($x);
-//echo round($x);
-//echo rand(1, 10);
+class User {
+  public $name;
 
-$s1 = "hell";
-$s2 = "くま";
-//;echo strlen($s1);
-//;echo mb_strlen($s2);
-//;printf("%s - %s - %.3f", $s1, $s2, $x);
+  public function __construct($name){
+    $this->name = $name;
+  }
 
-$colors = ["red", "pink", "orange"];
-echo count($colors);
-echo implode(",", $colors);
+  public function sayHi(){
+    echo "Hi, i am $this->name!";
+  }
+}
+
+$tom = new User("Tom");
+$bob = new User("Bob");
+
+echo $tom->name;
+$bob->sayHi();
