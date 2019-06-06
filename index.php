2e373b96f40a69afc5f687354eaddf3a1f2b5ab6
motomotoyoshi <motomoto.git@gmail.com>
2019-06-06 16:09:04 +0000

    form data

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php 
 
-function div($a, $b){
-  try {
-    if ( $b === 0) {
-      throw new Exception("cannot divide by 0!");
-    }
-    echo $a / $b;
-  } catch (Exception $e) {
-    echo $e->getMessage();
+$username = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = $_POST['username'];
+  $err = false;
+  if (strlen($username) > 8){
+    $err = true;
   }
 }
-
-div (7, 2);
-div (5, 0);
-
-
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  <form action="" method="POST">
+    <input type="text" name ="username" placeholder="user name" value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8' ); ?>">
+    <input type="submit" value="Check!">
+    <?php if ($err) { echo "Too long!"; } ?>
+  </form>
+</body>
+</html>
