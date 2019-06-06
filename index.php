8503c52b90e3d2bbd228c75fe8267b7aed4c0807
motomotoyoshi <motomoto.git@gmail.com>
2019-06-06 16:22:06 +0000

    cookie

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php 
 
-$username = '';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $username = $_POST['username'];
-  $err = false;
-  if (strlen($username) > 8){
-    $err = true;
-  }
-}
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-</head>
-<body>
-  <form action="" method="POST">
-    <input type="text" name ="username" placeholder="user name" value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8' ); ?>">
-    <input type="submit" value="Check!">
-    <?php if ($err) { echo "Too long!"; } ?>
-  </form>
-</body>
-</html>
+//setcookie("username", "foo", time()+60*60);
+setcookie("username", "foo", time()-60*60);
+
+echo $_COOKIE['username'];
