5367811bfa2c3240791dc5237723f81e16de0da7
motomotoyoshi <motomoto.git@gmail.com>
2019-06-26 16:05:47 +0000

    update index.php, learn php database

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php 
+<?php
 
-$testFile = "test.dat";
+define('DB_DATABASE', 'hoge_db');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', 'okipo121');
+define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' . DB_DATABASE);
 
-//$contents = file_get_contents($testFile);
-//$contents = file_get_contents("https://dotinstall.com");
-$contents = file($testFile);
-var_dump($contents);
+try {
+  // connect
+  $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // insert
+  $db->exec("inser33t into users (name, score) values ('oki', 88)");
+  echo "user added!";
+
+  // disconnect
+  $db = null;
+
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
