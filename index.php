a86416a09091a2913e2c5bc2c592a7a33a11f19a
motomotoyoshi <motomoto.git@gmail.com>
2019-06-26 17:14:20 +0000

    bindValue()

--- index.php
+++ index.php
@@ -11,8 +11,19 @@ try {
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   $stmt = $db->prepare("insert into users (name, score) values (?, ?)");
-  $stmt->execute(['okipo', 56]);
-  echo "inserted: " . $db->lastInsertId();
+  // $stmt->execute(['okipo', 56]);
+  // $stmt->execute(['okipo', 76]);
+  // $stmt->execute(['okipo', 26]);
+
+  $name = 'okipo';
+  $stmt->bindValue(1, $name, PDO::PARAM_STR);
+  // $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+  $score = 32;
+  $stmt->bindValue(2, $score, PDO::PARAM_INT);
+  $stmt->execute();
+  $score = 78;
+  $stmt->bindValue(2, $score, PDO::PARAM_INT);
+  $stmt->execute();
 
 } catch (PDOException $e) {
   echo $e->getMessage();
